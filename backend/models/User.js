96f2de33840c2d8e251d87c3736370173e2df93b
jyoti-gadgil3 <jyotig0699@gmail.com>
2025-03-10 22:25:13 +0530

    Creating a collection for new user

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,6 +21,5 @@ const userSchema = new Schema({
     },
   });
   const User = mongoose.model("user", userSchema);
-  User.createIndexes();
   module.exports = User;
   
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,22 +3,37 @@ const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
-// Creating a user using : POST "/api/auth". Doesn't require auth
+// Creating a user using : POST "/api/auth/createUser". No Login required
 router.post(
-  "/",
-  [body("name","Enter a valid name").isLength({ min: 3 }), body("email", "Enter a valid email").isEmail(), body("password", "Password must be atleast 5 characters").isLength({ min: 5 })],
-  (req, res) => {
+  "/createUser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    // If there are errors return Bad Request and the errors
     const errors = validationResult(req);
-     if (!errors.isEmpty()) {
-       return res.status(400).json({ errors: errors.array() });
-     }
-     User.create({
-         name: req.body.name,
-         password: req.body.password,
-         email: req.body.email,
-       }).then(user => res.json(user))
-       .catch(err=> {console.log(err)
-     res.json({error: 'Please enter a unique value for email', message: err.message})})
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // Check whether the user with this email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "Sorry a user with this email already exists" });
+      }
+      // Create a new user
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      });
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occured");
+    }
   }
 );
 
