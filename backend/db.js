5c7b8598e576dc33c6d511dd46c0138a5f71d6b7
jyoti-gadgil3 <jyotig0699@gmail.com>
2025-03-10 20:34:25 +0530

    Adding validations for the post request

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/?directConnection=true";
+const mongoURI = "mongodb://localhost:27017/inotebook?directConnection=true";
 
 async function connectToMongo() {
     await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
