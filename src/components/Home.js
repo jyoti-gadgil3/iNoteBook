674e525ca48ad67633fd0d5501098ee6db4dc90f
jyoti-gadgil3 <jyotig0699@gmail.com>
2025-03-19 13:12:35 +0530

    creating Notes, NoteItem

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
-import React, { useContext } from "react";
-import noteContext from "../context/notes/noteContext";
+import React from "react";
+import Notes from "./Notes"
 
 const Home = () => {
-  const context = useContext(noteContext);
-  const {notes, setNotes} = context
+  
   return (
     <div>
       <div className="container my-3">
@@ -36,13 +35,10 @@ const Home = () => {
             Submit
           </button>
         </form>
+        <Notes />
       </div>
-      <div className="container my-3">
-        <h2>Your Notes</h2>
-        {notes.map((note)=>{
-          return note.title
-        })}
-      </div>
+      
+      
     </div>
   );
 };
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -10,6 +10,68 @@ const notesInitial = [
       "tag": "personal",
       "date": "2025-03-17T10:08:25.726Z",
       "__v": 0
+    },
+    {
+      "_id": "67d7f4999486347520b278a4",
+      "user": "67cf29ea1f178e2bd81cb722",
+      "title": "New note",
+      "description": "complete the react course",
+      "tag": "work",
+      "date": "2025-03-17T10:08:25.726Z",
+      "__v": 0
+    },
+    {
+      "_id": "67d7f4999486347520b278a4",
+      "user": "67cf29ea1f178e2bd81cb722",
+      "title": "resume",
+      "description": "Start working on resume",
+      "tag": "work",
+      "date": "2025-03-17T10:08:25.726Z",
+      "__v": 0
+    },
+    {
+      "_id": "67d7f4999486347520b278a4",
+      "user": "67cf29ea1f178e2bd81cb722",
+      "title": "Grocery",
+      "description": "Get patatoes from the market",
+      "tag": "personal",
+      "date": "2025-03-17T10:08:25.726Z",
+      "__v": 0
+    }, {
+      "_id": "67d7f4999486347520b278a4",
+      "user": "67cf29ea1f178e2bd81cb722",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2025-03-17T10:08:25.726Z",
+      "__v": 0
+    },
+    {
+      "_id": "67d7f4999486347520b278a4",
+      "user": "67cf29ea1f178e2bd81cb722",
+      "title": "New note",
+      "description": "complete the react course",
+      "tag": "work",
+      "date": "2025-03-17T10:08:25.726Z",
+      "__v": 0
+    },
+    {
+      "_id": "67d7f4999486347520b278a4",
+      "user": "67cf29ea1f178e2bd81cb722",
+      "title": "resume",
+      "description": "Start working on resume",
+      "tag": "work",
+      "date": "2025-03-17T10:08:25.726Z",
+      "__v": 0
+    },
+    {
+      "_id": "67d7f4999486347520b278a4",
+      "user": "67cf29ea1f178e2bd81cb722",
+      "title": "Grocery",
+      "description": "Get patatoes from the market",
+      "tag": "personal",
+      "date": "2025-03-17T10:08:25.726Z",
+      "__v": 0
     }
   ]
 
