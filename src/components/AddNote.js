e00dafcd9bc21f1ac2f8b50ac939955c99a056db
jyoti-gadgil3 <jyotig0699@gmail.com>
2025-03-19 18:48:59 +0530

    Adding DeleteNote feature

---
+++ src/components/AddNote.js
@@ -0,0 +1,59 @@
+import React, { useContext, useState } from "react";
+import noteContext from "../context/notes/noteContext";
+
+const AddNote = () => {
+  const context = useContext(noteContext);
+  const { addNote } = context;
+  const [note, setNote] = useState({ title: "", description: "", tag: "" });
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    addNote(note.title, note.description, note.tag);
+  };
+
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="container my-3">
+      <h2>Add a Note</h2>
+      <form className="my-3">
+        <div className="form-group my-3 mb-3">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            name="title"
+            aria-describedby="emailHelp"
+            placeholder="Enter title"
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            className="form-control"
+            id="description"
+            name="description"
+            placeholder="Enter Description"
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={handleClick}>
+          Add Note
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddNote;
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,88 +1,114 @@
 import { useState } from "react";
 import NoteContext from "./noteContext";
 
-const notesInitial = [
-    {
-      "_id": "67d7f4999486347d520b278a4",
-      "user": "67cf29ea1f178e2bd81cb722",
-      "title": "My Title",
-      "description": "Please wake up early",
-      "tag": "personal",
-      "date": "2025-03-17T10:08:25.726Z",
-      "__v": 0
-    },
-    {
-      "_id": "67d7f49994863475fd20b278a4",
-      "user": "67cf29ea1f178e2bd81cb722",
-      "title": "New note",
-      "description": "complete the react course",
-      "tag": "work",
-      "date": "2025-03-17T10:08:25.726Z",
-      "__v": 0
-    },
-    {
-      "_id": "67d7f4999486347520b27sd8a4",
-      "user": "67cf29ea1f178e2bd81cb722",
-      "title": "resume",
-      "description": "Start working on resume",
-      "tag": "work",
-      "date": "2025-03-17T10:08:25.726Z",
-      "__v": 0
-    },
-    {
-      "_id": "67d7f4999486347xc520b278a4",
-      "user": "67cf29ea1f178e2bd81cb722",
-      "title": "Grocery",
-      "description": "Get patatoes from the market",
-      "tag": "personal",
-      "date": "2025-03-17T10:08:25.726Z",
-      "__v": 0
-    }, {
-      "_id": "67d7f49994863zsd47520b278a4",
-      "user": "67cf29ea1f178e2bd81cb722",
-      "title": "My Title",
-      "description": "Please wake up early",
-      "tag": "personal",
-      "date": "2025-03-17T10:08:25.726Z",
-      "__v": 0
-    },
-    {
-      "_id": "67d7f4999486347dfc520b278a4",
-      "user": "67cf29ea1f178e2bd81cb722",
-      "title": "New note",
-      "description": "complete the react course",
-      "tag": "work",
-      "date": "2025-03-17T10:08:25.726Z",
-      "__v": 0
-    },
-    {
-      "_id": "67d7f499xc9486347520b278a4",
-      "user": "67cf29ea1f178e2bd81cb722",
-      "title": "resume",
-      "description": "Start working on resume",
-      "tag": "work",
-      "date": "2025-03-17T10:08:25.726Z",
-      "__v": 0
-    },
-    {
-      "_id": "67d7f499948zxc6347520b278a4",
-      "user": "67cf29ea1f178e2bd81cb722",
-      "title": "Grocery",
-      "description": "Get patatoes from the market",
-      "tag": "personal",
-      "date": "2025-03-17T10:08:25.726Z",
-      "__v": 0
-    }
-  ]
-
-
 const NoteState = (props) => {
-    const [notes, setNotes] = useState(notesInitial)
-    return (
-        <NoteContext.Provider value={{notes, setNotes}}>
-            {props.children}
-        </NoteContext.Provider>
-    )
-}
+  const notesInitial = [
+    {
+      _id: "67d7f4999486347d520b278a4",
+      user: "67cf29ea1f178e2bd81cb722",
+      title: "My Title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2025-03-17T10:08:25.726Z",
+      __v: 0,
+    },
+    {
+      _id: "67d7f49994863475fd20b278a4",
+      user: "67cf29ea1f178e2bd81cb722",
+      title: "New note",
+      description: "complete the react course",
+      tag: "work",
+      date: "2025-03-17T10:08:25.726Z",
+      __v: 0,
+    },
+    {
+      _id: "67d7f4999486347520b27sd8a4",
+      user: "67cf29ea1f178e2bd81cb722",
+      title: "resume",
+      description: "Start working on resume",
+      tag: "work",
+      date: "2025-03-17T10:08:25.726Z",
+      __v: 0,
+    },
+    {
+      _id: "67d7f4999486347xc520b278a4",
+      user: "67cf29ea1f178e2bd81cb722",
+      title: "Grocery",
+      description: "Get patatoes from the market",
+      tag: "personal",
+      date: "2025-03-17T10:08:25.726Z",
+      __v: 0,
+    },
+    {
+      _id: "67d7f49994863zsd47520b278a4",
+      user: "67cf29ea1f178e2bd81cb722",
+      title: "My Title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2025-03-17T10:08:25.726Z",
+      __v: 0,
+    },
+    {
+      _id: "67d7f4999486347dfc520b278a4",
+      user: "67cf29ea1f178e2bd81cb722",
+      title: "New note",
+      description: "complete the react course",
+      tag: "work",
+      date: "2025-03-17T10:08:25.726Z",
+      __v: 0,
+    },
+    {
+      _id: "67d7f499xc9486347520b278a4",
+      user: "67cf29ea1f178e2bd81cb722",
+      title: "resume",
+      description: "Start working on resume",
+      tag: "work",
+      date: "2025-03-17T10:08:25.726Z",
+      __v: 0,
+    },
+    {
+      _id: "67d7f499948zxc6347520b278a4",
+      user: "67cf29ea1f178e2bd81cb722",
+      title: "Grocery",
+      description: "Get patatoes from the market",
+      tag: "personal",
+      date: "2025-03-17T10:08:25.726Z",
+      __v: 0,
+    },
+  ];
+
+  const [notes, setNotes] = useState(notesInitial);
+  // Add a Note
+  const addNote = (title, description, tag) => {
+    // Todo: API call
+    console.log("Adding a new note")
+    let note = {
+      _id: "67d7f4999rr4447520b278a4",
+      user: "67cf29ea1f178e2bd81cb722",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2025-03-17T10:08:25.726Z",
+      __v: 0,
+    };
+    setNotes(notes.concat(note))
+  }
+
+  // Delete a Note
+  const deleteNote = (id) => {
+    // Todo: API call
+    console.log("Deleting the note with id" + id);
+    const newNotes = notes.filter((note)=> {return note._id !== id})
+    setNotes(newNotes)
+  }
+
+  // Edit a Note
+  const editNote = (id, title, description, tag) => {
+
+  }
+
+
+  return <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>{props.children}</NoteContext.Provider>;
+};
 
 export default NoteState;
